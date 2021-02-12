7c1d3339f78e24470a351b697d194d106b7cac2e
Pawan Choure <pawanchoure@gmail.com>
2021-02-12 16:00:10 +0530

    Adding Missed TradeHandler

---
+++ src/main/kotlin/com/pawan/choure/handler/TradeHandler.kt
@@ -0,0 +1,19 @@
+package com.pawan.choure.handler
+
+import org.springframework.integration.annotation.ServiceActivator
+import org.springframework.integration.handler.AbstractMessageHandler
+import org.springframework.messaging.Message
+import org.springframework.messaging.MessageHandler
+import org.springframework.messaging.MessagingException
+import org.springframework.stereotype.Component
+
+@Component
+open class TradeHandler : MessageHandler {
+    @Throws(MessagingException::class)
+    //@ServiceActivator(inputChannel = "inputChannelTradeServiceActivator",outputChannel = "nullChannel")  No Use
+    open override fun handleMessage(message: Message<*>) {
+        println("TradeHandler Header" + message.headers)
+        println("TradeHandler PayLoad Hello" +message.payload)
+        println("TradeHandler MessageToString$message")
+    }
+}
